361a9698ced1ef3ee78e89eca65829fd6ac9a887
Juhani <arsmir@google.com>
2023-09-08 01:06:48 +0300

    t1

--- t1/file.py
+++ t1/file.py
@@ -1,3 +1,29 @@
 #!/usr/bin/python3
+import os, re, nltk, json
 
+nltk.download('punkt')
+path = 'texts/'
+bigText = ''
 
+# read all the files
+for book in os.listdir(path):
+    with open(path + book, "r") as f:
+        bigText += f.read() + ' '
+
+# tokenize text
+tokens = [nltk.word_tokenize(t) for t in nltk.sent_tokenize(bigText)]
+filtetedTokens = []
+r = re.compile("[а-яА-Я]+")
+
+# filter tokens only for cyrillic words
+for token in tokens:
+    filtetedTokens += [w.lower() for w in filter(r.match, token)]
+
+# print(set(filtetedTokens))
+
+# leave only unique words
+filtetedTokens = list(set(filtetedTokens))
+
+# save tokens as json
+with open('words.json', 'w') as f:
+    f.write(json.dumps({'words': filtetedTokens}))
---
+++ t1/spellchecker.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+import json, os
+
+# read tokens
+with open('words.json') as f:
+    tokens = json.load(f)['words']
+
+# print(tokens)
+alphabet = 'абвгдеёжзийклмнопрстуфчцчшщъыьэюя'
+
+# spell checker
+def checkSpelling(word):
+    word = word.lower()
+    if word in tokens:
+        return word, 'No error!'
+
+    matches = []
+
+    #adding letter
+    for i in range(len(word)+1):
+        for letter in alphabet:
+            match = word[:i] + letter + word[i:]
+            if match in tokens:
+                matches.append(match)
+    #removing letter
+    for i in range(len(word)):
+        match = word[:i] + word[i+1:]
+        if match in tokens:
+            matches.append(match)
+    #replace letter
+    for i in range(len(word)):
+        for letter in alphabet:
+            match = word[:i] + letter + word[i+1:]
+            if match in tokens:
+                matches.append(match)
+    #swap two letters
+    for i in range(len(word)-1):
+        wordList = list(word)
+        wordList[i], wordList[i+1] = wordList[i+1], wordList[i]
+        match = "".join(wordList)
+        if match in tokens:
+            matches.append(match)
+    if len(matches) > 0:
+        return matches[0], matches
+    else:
+        return word.upper(), 'No matches!'
+
+out = ''
+prompt = ''
+# start main loop
+os.system('clear')
+print('Welcome to russian spell checker!')
+while True:
+    prompt += out + ' '
+    out = input(prompt + '>')
+    if out == '':
+        break
+    out, msg = checkSpelling(out)
+    os.system('clear')
+    print(msg)
