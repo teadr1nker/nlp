b4f07188ed5796bac469be2ddb1961d7036c0322
Juhani <arsmir999@google.com>
2023-09-24 22:29:28 +0300

    t2

--- t2/getVectors.sh
+++ t2/getVectors.sh
@@ -1,2 +1,4 @@
+mkdir vectors
+cd vectors
 wget "https://dl.fbaipublicfiles.com/fasttext/vectors-crawl/cc.en.300.vec.gz"
 wget "https://dl.fbaipublicfiles.com/fasttext/vectors-crawl/cc.ru.300.vec.gz"
