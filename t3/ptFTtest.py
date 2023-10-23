6fe5d872a40613190bf67a7db854c53a73e5a7e2
Juhani <arsmir999@google.com>
2023-10-23 15:34:26 +0300

    t3

--- t3/ptFTtest.py
+++ t3/ptFTtest.py
@@ -1,4 +1,20 @@
 #!/usr/bin/python3
-from gensim.models import FastText
+import fasttext
 
-model = FastText.load_fasttext_format('../t2/vectors/cc.ru.300.bin')
+model = fasttext.load_model('../t2/vectors/cc.ru.300.bin')
+
+print(model.get_nearest_neighbors('Россия'))
+def test(arr):
+    print(f'\npos: {arr[:-1],} neg: {arr[-1:]}')
+    print(model.get_analogies(arr[0], arr[2], arr[1]))
+
+test(["король", "женщина", "мужчина"])
+test(["программист", "музыка", "код"])
+test(["сон", "испуг", "хороший"])
+test(["исследования", "искусство", "наука"])
+test(["Россия", "Токио", "Москва"])
+test(["голова", "мозг", "волосы"])
+test(["ночь", "свет", "темнота"])
+test(["бекон", "пить", "есть"])
+test(['сосиска', "большая", "маленькая"])
+test(["вода", "душа", "тело"])
